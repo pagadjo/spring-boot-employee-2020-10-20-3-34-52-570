0506aa1910030106c30453a8557fa37aa15af59b
Cedric Don Coloma <colomce@oocl.com>
2020-10-21 14:35:41 +0800

    [Employee][Cedric-JC] feature: Added functionality to list all employees

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.models.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+
+    private static final List<Employee> employees = new ArrayList<>();
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employees;
+    }
+}
