0e86d544775c77f48aafa0e9770f747bae8fa7e3
Cedric Don Coloma <colomce@oocl.com>
2020-10-21 14:51:22 +0800

    [Employee][Cedric-JC] feature: Added feature to update the employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,5 +38,16 @@ public class EmployeesController {
                 .filter(employee -> employee.getId().equals(employeeId))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @PutMapping("/{employeeId}")
+    public Employee update(@PathVariable("employeeId") String employeeId, @RequestBody Employee updatedEmployee) {
+        employees.stream()
+                .filter(employee -> employee.getId().equals(employeeId))
+                .findFirst().ifPresent(employee -> {
+                    employees.remove(employee);
+                    employees.add(updatedEmployee);
+                });
+        return updatedEmployee;
     }
 }
