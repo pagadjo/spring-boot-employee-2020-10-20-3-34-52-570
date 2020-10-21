cca6fce6f7037499b6e36e2434ef381e9d95156c
Cedric Don Coloma <colomce@oocl.com>
2020-10-21 14:38:56 +0800

    [Employee][Cedric-JC] feature: Added functionality to create an employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,11 @@ public class EmployeesController {
     @GetMapping
     public List<Employee> getAll() {
         return employees;
+    }
+
+    @PostMapping
+    public Employee create(@RequestBody Employee employee) {
+        employees.add(employee);
+        return employee;
     }
 }
