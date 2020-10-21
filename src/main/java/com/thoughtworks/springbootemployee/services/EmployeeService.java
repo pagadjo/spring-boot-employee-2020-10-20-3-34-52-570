4ebedd669da2f7c3180376e65c0eb282b65f5d94
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-21 16:54:07 +0800

    [Employee Service][Cedric-JC]feature: implemented getAll() in EmployeeService

--- src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
@@ -6,10 +6,14 @@ import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
 import java.util.List;
 
 public class EmployeeService {
-    public EmployeeService(EmployeeRepository repository) {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     public List<Employee> getAll() {
-        return null;
+        return employeeRepository.getAll();
     }
 }
