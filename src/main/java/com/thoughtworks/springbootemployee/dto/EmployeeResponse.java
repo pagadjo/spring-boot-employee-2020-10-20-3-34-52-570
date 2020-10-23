ac0115d9e4e03214997d0581479706d46c591aa2
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-23 14:21:03 +0800

    [JPA][Charlie-JC]feature: created EmployeeRequest and EmployeeResponse

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponse.java
@@ -0,0 +1,75 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class EmployeeResponse {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+    private Integer companyId;
+
+    public EmployeeResponse() {
+    }
+
+    public EmployeeResponse(Integer id, String name, Integer age, String gender, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+}
