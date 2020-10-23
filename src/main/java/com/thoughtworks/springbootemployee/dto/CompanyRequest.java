f597f855be13aaade303a950b8c698596fc72774
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-23 15:01:10 +0800

    [JPA][Charlie-JC]feature: added CompanyRequest and CompanyResponse

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyRequest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class CompanyRequest {
+
+    private String companyName;
+    public CompanyRequest() {
+    }
+
+    public CompanyRequest(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyResponse.java
@@ -0,0 +1,50 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import com.thoughtworks.springbootemployee.models.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyResponse {
+
+    private Integer companyId;
+    private String companyName;
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public CompanyResponse() {
+    }
+
+    public CompanyResponse(Integer companyId, String companyName) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public int getEmployeeNumber() {
+        return employees.size();
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
