eb290105cc7dd58f3ecc3a3febfc2b2099c6af50
BORBECH <charlieborbs18@gmail.com>
2020-10-23 13:09:01 +0800

    [JPA][Charlie-JC]feature: apply InvalidEmployeeException in EmployeeService

--- src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.springbootemployee.services;
 
 import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
+import com.thoughtworks.springbootemployee.exception.InvalidEmployeeException;
 import com.thoughtworks.springbootemployee.models.Employee;
 import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
-import com.thoughtworks.springbootemployee.repositories.EmployeeRepositoryLegacy;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -32,30 +32,30 @@ public class EmployeeService {
 
     public Employee searchById(Integer id) {
         return employeeRepository.findById(id)
-                .orElseThrow(()->new EmployeeNotFoundException("Employee " + id + " not found!"));
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee " + id + " not found!"));
     }
 
     public Employee update(Integer id, Employee employeeUpdate) {
-        Employee employee = employeeRepository.findById(id).orElse(null);
-        if (nonNull(employee)) {
-            if (nonNull(employeeUpdate.getName())) {
-                employee.setName(employeeUpdate.getName());
-            }
-            if (nonNull(employeeUpdate.getAge())) {
-                employee.setAge(employeeUpdate.getAge());
-            }
-            if (nonNull(employeeUpdate.getGender())) {
-                employee.setGender(employeeUpdate.getGender());
-            }
-            if (nonNull(employeeUpdate.getSalary())) {
-                employee.setSalary(employeeUpdate.getSalary());
-            }
-            if (nonNull(employeeUpdate.getCompanyId())) {
-                employee.setCompanyId(employeeUpdate.getCompanyId());
-            }
-            return employeeRepository.save(employee);
+        Employee employee = searchById(id);
+        validateEmployee(employee);
+        employee.setName(employeeUpdate.getName());
+        employee.setGender(employeeUpdate.getGender());
+        if (nonNull(employeeUpdate.getAge())) {
+            employee.setAge(employeeUpdate.getAge());
         }
-        throw new EmployeeNotFoundException("Employee " + id + " not found!");
+        if (nonNull(employeeUpdate.getSalary())) {
+            employee.setSalary(employeeUpdate.getSalary());
+        }
+        if (nonNull(employeeUpdate.getCompanyId())) {
+            employee.setCompanyId(employeeUpdate.getCompanyId());
+        }
+        return employeeRepository.save(employee);
+    }
+
+    public void validateEmployee(Employee employee) {
+        if (isNull(employee.getName()) || isNull(employee.getGender())) {
+            throw new InvalidEmployeeException("Employee data is not valid!");
+        }
     }
 
     public void delete(Integer id) {
