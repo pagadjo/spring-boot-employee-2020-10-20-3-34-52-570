1eb822af65d3b43a9a1a78fa4ea5a13ceb2f86aa
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-23 11:47:34 +0800

    [JPA][Charlie-JC]feature: added EmployeeNotFoundException

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
@@ -0,0 +1,6 @@
+package com.thoughtworks.springbootemployee.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+    }
+}
